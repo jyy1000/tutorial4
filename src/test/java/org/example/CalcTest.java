8fdd5ad8c8ae986bcefb6470ec2f103d3e574fe4
jyy1000 <2485210592@qq.com>
2024-09-25 17:52:15 +0800

    a

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -10,5 +10,8 @@ public class CalcTest {
     void testAddition() {
         assertEquals(4, c.add(2,2));
     }
-
+    void testSubtraction() {
+        Calculator c = new Calculator();
+        assertEquals(2, c.subtract(4, 2));
+    }
 }
